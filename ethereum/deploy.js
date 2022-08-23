d1f14e76719744bd1233afa96a0d18521107550c
guney bilen <guneybilen@yahoo.com>
2022-08-23 13:30:01 -0400

    initial commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'legend monkey crash olympic buzz that reveal ten advance march word earth',
+  'https://rinkeby.infura.io/v3/3a02eadf23cb4cccb17ad98d015cdf59'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
